24a8a200cdb3fc130eee7a2051e12f1c451bc73f
S.Pannawit <6210612682@student.tu.ac.th>
2021-09-24 11:07:31 +0000

    intitial add

--- quota_Register/settings.py
+++ quota_Register/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
-import django_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -134,5 +134,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
+import django_heroku
 django_heroku.settings(locals())
